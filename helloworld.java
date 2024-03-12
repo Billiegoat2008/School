e33051417c51a112b579d1ea7b3ffe617a3af705
Billiegoat2008 <156938498+Billiegoat2008@users.noreply.github.com>
2024-03-12 10:04:36 -0700

    Add files via upload
    My hello world program

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+// Writen by Billie CHrleston, And Kim Gross
+// My fisdt hello wrold program
+// Programming development
+public class helloworld {
+  public static void main(String[] args) {
+    System.out.println("Hello world");// Prints hello world to the console
+    System.out.println("My name is Billie Charleston");
+    System.out.println("And I have 15 Dogs and 3 cats");
+    System.out.println("ANd I Also have two sister and one brother");
+  }
+}
